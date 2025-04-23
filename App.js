a1a1f33282b475eb8c710f6f29c1917475bdd331
Steven <110705030+youesei@users.noreply.github.com>
2025-04-23 14:49:25 -0500

    Implementación de navegación con Drawer y Stack en la aplicación. Se agregaron tres pantallas (ScreenA, ScreenB, ScreenC) y se actualizaron las dependencias de React Navigation y Expo en package.json y package-lock.json.

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.tsx
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Button, View, Text } from 'react-native';
 
-export default function App() {
+const Drawer = createDrawerNavigator();
+const StackOne = createNativeStackNavigator();
+const StackTwo = createNativeStackNavigator();
+
+const ScreenA = ({ navigation }) => (
+  <View>
+    <Text>Screen A</Text>
+    <Button title="Go to Screen C as soon as you start the app" onPress={() => navigation.navigate('StackTwo', { screen: 'ScreenC' })} />
+  </View>
+);
+
+const ScreenB = ({ navigation }) => (
+  <View>
+    <Text>Screen B</Text>
+    <Button title="Go to Screen C" onPress={() => navigation.navigate('ScreenC')} />
+  </View>
+);
+
+const ScreenC = () => (
+  <View>
+    <Text>Screen C</Text>
+  </View>
+);
+
+function StackOneNavigator() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <StackOne.Navigator>
+      <StackOne.Screen name="ScreenA" component={ScreenA} />
+    </StackOne.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function StackTwoNavigator() {
+  return (
+    <StackTwo.Navigator>
+      <StackTwo.Screen name="ScreenB" component={ScreenB} />
+      <StackTwo.Screen name="ScreenC" component={ScreenC} />
+    </StackTwo.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="StackOne">
+        <Drawer.Screen name="StackOne" component={StackOneNavigator} />
+        <Drawer.Screen name="StackTwo" component={StackTwoNavigator} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
